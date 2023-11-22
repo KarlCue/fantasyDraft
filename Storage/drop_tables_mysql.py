6849ba044d32c4a62c105bad637baec92a7e057f
KarlCue <kacue38@gmail.com>
2023-11-21 16:02:40 -0800

    user=user stuff

--- Storage/drop_tables_mysql.py
+++ Storage/drop_tables_mysql.py
@@ -1,6 +1,6 @@
 import mysql.connector
 
-db_conn = mysql.connector.connect(host="acit3855.eastus.cloudapp.azure.com", user="root",
+db_conn = mysql.connector.connect(host="acit3855.eastus.cloudapp.azure.com", user="user",
 password="password", database="FantasyDraft")
 
 db_cursor = db_conn.cursor()
