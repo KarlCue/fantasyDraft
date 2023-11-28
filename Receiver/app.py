41786516bd5a896ca377e66f7c4959ced708866e
KarlCue <kacue38@gmail.com>
2023-11-27 21:38:38 -0800

    old stuff

--- Receiver/app.py
+++ Receiver/app.py
@@ -8,7 +8,7 @@ import requests
 import json
 from pykafka import KafkaClient
 from connexion import NoContent
-import time
+
 current_datetime = datetime.datetime.now()
 current_datetime_str = current_datetime.strftime("%Y-%m-%d %H:%M:%S")
 
@@ -19,45 +19,18 @@ CONF_YML = 'app_conf.yml'
 LOG_YML = 'log_conf.yml'
 HOST = 'acit3855.eastus.cloudapp.azure.com:9092'
 
-with open(CONF_YML, "r") as f:
+with open (CONF_YML, "r") as f:
     app_config = yaml.safe_load(f.read())
 
-with open(LOG_YML, 'r') as f:
+with open (LOG_YML, 'r') as f:
     log_config = yaml.safe_load(f.read())
     logging.config.dictConfig(log_config)
 
 logger = logging.getLogger('basicLogger')
 
-# Initialize Kafka client and topic
-kafka_client = None
-kafka_topic = None
-
-def connect_to_kafka():
-    global kafka_client
-    global kafka_topic
-    max_retries = app_config["kafka"]["max_retries"]
-    retry_count = 0
-
-    while retry_count < max_retries:
-        try:
-            kafka_client = KafkaClient(hosts=HOST)
-            kafka_topic = kafka_client.topics[str.encode('FantasyDraft')]
-            logger.info("Connected to Kafka successfully.")
-            break
-        except Exception as e:
-            logger.error(f"Error connecting to Kafka: {str(e)}")
-            retry_count += 1
-            logger.info(f"Retrying connection to Kafka. Retry count: {retry_count}")
-            time.sleep(app_config["kafka"]["retry_interval"])
-
-    if retry_count == max_retries:
-        logger.error("Failed to connect to Kafka after maximum retries. Exiting.")
-        # Optionally, you may choose to raise an exception or take other actions.
-
-# Connect to Kafka on service startup
-connect_to_kafka()
 
 def add_pick(body):
+
     trace_id = str(uuid.uuid4())
 
     body["trace_id"] = trace_id
@@ -97,6 +70,7 @@ def add_trade(body):
     logger.info(f'Returned Pick response (id: {body["trace_id"]}) with status code 201')
 
     return NoContent, 201
+
 
 app = connexion.FlaskApp(__name__, specification_dir='')
 app.add_api(YAML_FILE, 
